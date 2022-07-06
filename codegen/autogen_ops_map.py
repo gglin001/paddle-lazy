f8076d974e31206edf8d0c7376f28008649f567e
Allen Guo <alleng@graphcore.ai>
2022-07-06 09:11:17 +0000

    add autogen_ops_map

---
+++ codegen/autogen_ops_map.py
@@ -0,0 +1,103 @@
+import argparse
+
+import yaml
+from base_api import BaseAPI
+
+
+def gen_cc(cc, cc1):
+    return f"""
+#include <map>
+#include "paddle_lazy/eager_backend/autogen_ops.h"
+#include "paddle_lazy/lazy_backend.h"
+#include "paddle_lazy/lazy_nodes.h"
+
+namespace phi {{
+
+{cc}
+
+static std::map<std::string, std::function<void(LazyNodePtr)>> dense_map {{
+
+{cc1}
+
+}};
+
+}}  // namespace phi
+
+"""
+
+
+def generate_api(ccs, ccs1, args):
+    cc = ''.join(ccs)
+    cc1 = ''.join(ccs1)
+    cc = gen_cc(cc, cc1)
+
+    with open(args.api_source_path, 'w') as f:
+        f.write(cc)
+
+
+class API(BaseAPI):
+
+    def __init__(self, api_yaml) -> None:
+        super().__init__(api_yaml)
+        self.api_node = f"{self.api.capitalize()}LazyNode"
+        self.api_kernel = f"{self.api.capitalize()}Kernel"
+        self.lambda_name = f"lambda_{self.api}"
+        self.dense_name = f"dense_{self.api}"
+
+    def cc(self):
+        api_code = f"""
+auto lambda_{self.api} = [](LazyNodePtr base_node) {{
+  auto node = static_cast<{self.api_node}*>(base_node.get());
+  return {self.dense_name}({self.get_dense_args()});
+}};
+"""
+
+        # {"abs", lambda_abs},
+        dense_reg = f"""{{"{self.api}", {self.lambda_name}}},
+"""
+        return api_code, dense_reg
+
+    def get_dense_args(self):
+        code = []
+
+        for idx, _ in enumerate(self.inputs['names']):
+            code.append(f"node->ins{[idx]}->GetDenseTensor()")
+
+        for idx, _ in enumerate(self.outputs['names']):
+            code.append(f"node->outs{[idx]}->GetDenseTensor()")
+
+        for name in self.attrs['names']:
+            code.append(f"node->{name}")
+
+        code = ', '.join(code)
+        return code
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Generate PaddlePaddle C++ API files')
+    parser.add_argument('--api_yaml_path',
+                        help='path to api yaml file',
+                        nargs='+',
+                        default=f'codegen/test_api.yaml')
+    parser.add_argument('--api_source_path',
+                        help='output of generated api source code file',
+                        default='paddle_lazy/eager_backend/ops_map.h')
+    args = parser.parse_args()
+    # args.api_yaml_path = list(args.api_yaml_path)[0]
+
+    with open(args.api_yaml_path, 'r') as f:
+        yaml_list = yaml.load(f, Loader=yaml.FullLoader)
+
+    ccs = []
+    ccs1 = []
+    for api_yaml in yaml_list:
+        api = API(api_yaml)
+        cc, cc1 = api.cc()
+        ccs.append(cc)
+        ccs1.append(cc1)
+    generate_api(ccs, ccs1, args)
+
+
+if __name__ == '__main__':
+    main()
--- paddle_lazy/eager_backend/op_runner.cc
+++ paddle_lazy/eager_backend/op_runner.cc
@@ -1,6 +1,7 @@
 #include "paddle_lazy/eager_backend/op_runner.h"
 
 #include "paddle_lazy/eager_backend/all_ops.h"
+#include "paddle_lazy/eager_backend/ops_map.h"
 #include "paddle_lazy/lazy_nodes.h"
 
 namespace phi {
@@ -12,39 +13,11 @@ void OpRunner::Run(phi::LazyNodePtr node) {
         node->GetDenseTensor(), CPUPlace(), false, node->GetDenseTensor());
   }
 
-  if (node->op_type == "abs") {
-    dense_abs(node->ins.front()->GetDenseTensor(),
-              node->outs.front()->GetDenseTensor());
-  } else if (node->op_type == "conv2d") {
-    auto conv2d_node = static_cast<Conv2dLazyNode *>(node.get());
-    dense_conv2d(node->ins[0]->GetDenseTensor(),
-                 node->ins[1]->GetDenseTensor(),
-                 conv2d_node->outs.front()->GetDenseTensor(),
-                 conv2d_node->strides,
-                 conv2d_node->dilations,
-                 conv2d_node->paddding_algorithm,
-                 conv2d_node->groups,
-                 conv2d_node->dilations,
-                 conv2d_node->data_format,
-                 conv2d_node->use_addto,
-                 conv2d_node->workspace_size_MB,
-                 conv2d_node->exhaustive_search);
-  } else if (node->op_type == "pool2d") {
-    auto pool2d_node = static_cast<Pool2dLazyNode *>(node.get());
-    dense_pool2d(node->ins[0]->GetDenseTensor(),
-                 pool2d_node->outs.front()->GetDenseTensor(),
-                 pool2d_node->kernel_size,
-                 pool2d_node->strides,
-                 pool2d_node->paddings,
-                 pool2d_node->ceil_mode,
-                 pool2d_node->exclusive,
-                 pool2d_node->data_format,
-                 pool2d_node->pooling_type,
-                 pool2d_node->global_pooling,
-                 pool2d_node->adaptive,
-                 pool2d_node->padding_algorithm);
+  if (dense_map.count(node->op_type) == 0) {
+    PADDLE_THROW(
+        phi::errors::Unimplemented("not supported op: %s", node->op_type));
   } else {
-    PADDLE_THROW(phi::errors::Unimplemented("not suported op"));
+    dense_map.at(node->op_type)(node);
   }
 }
 
