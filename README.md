b3f67e38e89923c0133a9896d43dbd4e474e2336
Allen Guo <alleng@graphcore.ai>
2022-08-15 10:06:29 +0000

    update readme

--- README.md
+++ README.md
@@ -1,12 +1,3 @@
 # Paddle Lazy Tensor(for IPU)
 
 WIP
-
-## TODO
-
-- direct use cpu kernel for registration
-- auto generate lazyNodes, kernels, eager_ops
-- add Box function to unify API
-- support backward ops
-- use a real IR(MLIR)
-- others
--- mlir_lib/paddle-lsp-server/paddle-lsp-server.cpp
+++ mlir_lib/paddle-lsp-server/paddle-lsp-server.cpp
@@ -1,8 +1,9 @@
-#include "Paddle/PaddleDialect.h"
 #include "mlir/IR/Dialect.h"
 #include "mlir/IR/MLIRContext.h"
 #include "mlir/InitAllDialects.h"
 #include "mlir/Tools/mlir-lsp-server/MlirLspServerMain.h"
+
+#include "Paddle/PaddleDialect.h"
 
 int main(int argc, char **argv) {
   mlir::DialectRegistry registry;
