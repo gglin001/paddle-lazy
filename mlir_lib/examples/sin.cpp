998946c23aa110e43b1ab94f1b5c6fffb9a250ac
Allen Guo <alleng@graphcore.ai>
2022-08-18 10:57:41 +0800

    add cpp example (#8)

---
+++ mlir_lib/examples/sin.cpp
@@ -0,0 +1,54 @@
+#include "mlir/Dialect/Arithmetic/IR/Arithmetic.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Dialect/Math/IR/Math.h"
+#include "mlir/IR/AsmState.h"
+#include "mlir/IR/BuiltinOps.h"
+#include "mlir/IR/Dialect.h"
+#include "mlir/IR/MLIRContext.h"
+#include "mlir/IR/OperationSupport.h"
+#include "mlir/IR/SymbolTable.h"
+#include "mlir/IR/Value.h"
+#include "mlir/InitAllDialects.h"
+#include "mlir/InitAllPasses.h"
+#include "mlir/Parser/Parser.h"
+#include "mlir/Pass/PassManager.h"
+#include "mlir/Support/FileUtilities.h"
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/ErrorHandling.h"
+#include "llvm/Support/InitLLVM.h"
+#include "llvm/Support/SourceMgr.h"
+#include "llvm/Support/ToolOutputFile.h"
+
+#include "Paddle/PaddleDialect.h"
+#include "Paddle/PaddleOps.h"
+
+using namespace mlir;
+using namespace llvm;
+using namespace mlir::paddle;
+using namespace mlir::paddle::Paddle;
+
+int main() {
+  mlir::registerAllPasses();
+  DialectRegistry registry;
+  registerAllDialects(registry);
+  registry.insert<PaddleDialect>();
+
+  MLIRContext context;
+  OwningOpRef<mlir::ModuleOp> module =
+      ModuleOp::create(mlir::UnknownLoc::get(&context));
+  context.appendDialectRegistry(registry);
+  context.loadAllAvailableDialects();
+
+  // llvm::SourceMgr sourceMgr;
+  // mlir::SourceMgrDiagnosticHandler sourceMgrHandler(sourceMgr, &context);
+
+  OpBuilder builder(module->getBodyRegion());
+
+  auto zeroAttr = builder.getZeroAttr(builder.getF32Type());
+  mlir::Value zero = builder.create<arith::ConstantOp>(
+      mlir::UnknownLoc::get(&context), zeroAttr);
+  builder.create<Paddle::SinOp>(mlir::UnknownLoc::get(&context),
+                                builder.getF32Type(), zero);
+
+  module->dump();
+}
